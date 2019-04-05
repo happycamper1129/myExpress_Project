6ccaaf00bf493582cf8918a6b4c1187f4d11e7e1
happycamper1129 <happycamper.sw@gmail.com>
2019-04-05 23:20:57 +0200

    register jwt schema on oauth2 call

--- test/services/users.test.js
+++ test/services/users.test.js
@@ -224,7 +224,6 @@ describe('User service tests', () => {
       user = createRandomUserObject();
       return userService.insert(user);
     }).then(newUser => {
-      user = newUser; // update test user
       should.exist(newUser);
       user.id = newUser.id;
     }));
