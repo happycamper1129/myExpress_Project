af214101c2316b06bc20d270c4243c79e09815f4
happycamper1129 <happycamper.sw@gmail.com>
2019-04-05 13:28:02 -0500

    Updated validateAndCreateUser & validateAndCreateApp in user & application services.

--- test/e2e/oauth2-authorization-code.js
+++ test/e2e/oauth2-authorization-code.js
@@ -35,7 +35,7 @@ describe('oauth2 authorization code grant type', () => {
           path: '/not-found'
         }
       },
-      policies: ['cors', 'jwt', 'basic-auth', 'oauth2', 'proxy'],
+      policies: ['oauth2', 'proxy'],
       pipelines: {
         ping: {
           apiEndpoints: ['ping'],
